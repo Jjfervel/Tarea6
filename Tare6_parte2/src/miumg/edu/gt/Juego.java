7037355db78589999b8fbe8b2313bfe2a268c9f2
Jjfervel <89677578+Jjfervel@users.noreply.github.com>
2021-08-27 23:18:54 -0600

    Add files via upload

---
+++ Tare6_parte2/src/miumg/edu/gt/Juego.java
@@ -0,0 +1,7 @@
+package miumg.edu.gt;
+
+public interface Juego {
+    void iniciar ();
+    void jugar ();
+    void finalizar();
+}
---
+++ Tare6_parte2/src/miumg/edu/gt/main.java
@@ -0,0 +1,11 @@
+package miumg.edu.gt;
+
+public class main {
+    public static void main(String[] args) {
+        Juegos juego1 = new Juegos();
+        juego1.iniciar();
+        juego1.jugar();
+        juego1.finalizar();
+
+    }
+}
